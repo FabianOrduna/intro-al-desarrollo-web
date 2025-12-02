e2742994be5c5b2d2f875aba0dfeee3bf6d44b24
SignLingua <signlingua@outlook.com>
2025-12-02 01:10:22 -0600

    Contador

--- React/hello-world-react/src/components/Contador/ContadorConUseState.jsx
+++ React/hello-world-react/src/components/Contador/ContadorConUseState.jsx
@@ -6,6 +6,18 @@ const ContadorConUseState = () => {
   return (
     <div>
       <p>Valor actual del contador: {contador}</p>
+      <button
+        onClick={() => {
+          setContador(contador + 1);
+          setContador(contador + 1);
+          setContador(contador + 1);
+        }}
+      >
+        Sumar 3 (versión insegura)
+      </button>
+      <button onClick={() => setContador(contador + 1)}>+1</button>
+      <button onClick={() => setContador(contador + 5)}>+5</button>
+      <button onClick={() => setContador(contador + 10)}>+10</button>
       <button onClick={() => setContador((anterior) => anterior + 1)}>
         Incrementa +1
       </button>
